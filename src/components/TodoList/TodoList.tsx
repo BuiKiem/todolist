162de1cbaf4b118a2cfd2cfb9f13abd053e4ec05
Bui Kiem <buivantrung1105@gmail.com>
2020-04-12 21:31:30 +0700

    display empty and complete status

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-
-import { TodoItem } from "../TodoItem/TodoItem";
 import {
   ExpansionPanel,
   ExpansionPanelSummary,
   List,
   Typography,
 } from "@material-ui/core";
+import {
+  InboxOutlined as InboxOutlinedIcon,
+  DoneAllOutlined as DoneAllOutlinedIcon,
+} from "@material-ui/icons";
+
+import { TodoItem } from "../TodoItem/TodoItem";
 
 import { toggleTodo } from "../../reducers/todos";
 
@@ -21,8 +25,54 @@ export const TodoList: React.FC<IProps> = ({ todos }) => {
   const onGoingTodos = todos.filter((todo) => !todo.complete);
   const completedTodos = todos.filter((todo) => todo.complete);
 
-  return (
-    <div>
+  const empty = (
+    <div className="flex flex-col items-center">
+      <InboxOutlinedIcon color="disabled" fontSize="large" />
+      <div className="text-center">
+        <Typography variant="subtitle2" component="h2">
+          <span className="capitalize">your</span> list is empty
+        </Typography>
+        <Typography variant="caption" component="h3">
+          <span className="capitalize">anything</span> to add?
+        </Typography>
+      </div>
+    </div>
+  );
+
+  const welldone = (
+    <>
+      <ExpansionPanel>
+        <ExpansionPanelSummary>
+          <Typography variant="subtitle1">
+            Completed ({completedTodos.length})
+          </Typography>
+        </ExpansionPanelSummary>
+        <List>
+          {completedTodos.map((todo) => (
+            <TodoItem
+              key={todo.text}
+              todo={todo}
+              toggleTodo={() => dispatch(toggleTodo(todo.text))}
+            />
+          ))}
+        </List>
+      </ExpansionPanel>
+      <div className="flex flex-col items-center">
+        <DoneAllOutlinedIcon color="disabled" fontSize="large" />
+        <div className="text-center">
+          <Typography variant="subtitle2" component="h2">
+            <span className="capitalize">well</span> done
+          </Typography>
+          <Typography variant="caption" component="h3">
+            <span className="capitalize">you</span> have completed all todos
+          </Typography>
+        </div>
+      </div>
+    </>
+  );
+
+  const list = (
+    <>
       <List>
         {onGoingTodos.map((todo) => (
           <TodoItem
@@ -32,7 +82,6 @@ export const TodoList: React.FC<IProps> = ({ todos }) => {
           />
         ))}
       </List>
-
       {completedTodos.length > 0 && (
         <ExpansionPanel>
           <ExpansionPanelSummary>
@@ -51,6 +100,16 @@ export const TodoList: React.FC<IProps> = ({ todos }) => {
           </List>
         </ExpansionPanel>
       )}
+    </>
+  );
+
+  return (
+    <div>
+      {onGoingTodos.length === 0
+        ? completedTodos.length === 0
+          ? empty
+          : welldone
+        : list}
     </div>
   );
 };
